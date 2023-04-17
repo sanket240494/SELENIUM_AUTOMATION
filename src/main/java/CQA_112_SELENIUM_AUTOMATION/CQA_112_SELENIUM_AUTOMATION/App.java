f81d5cc28f0e83d4deaa7fc4a03e2f00dff8eee1
sanket240494 <sanketladke007@gmail.com>
2023-04-17 20:34:46 +0530

    Selenium Automation

---
+++ src/main/java/CQA_112_SELENIUM_AUTOMATION/CQA_112_SELENIUM_AUTOMATION/App.java
@@ -0,0 +1,13 @@
+package CQA_112_SELENIUM_AUTOMATION.CQA_112_SELENIUM_AUTOMATION;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
